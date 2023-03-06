a482f78848c0ab5c4d4db5b8a572dab92859f6fa
Hjun1004 <hjun1004@naver.com>
2023-03-06 23:44:36 +0900

    명언등 등록시 마다 번호 증가

--- src/test/java/com/ll/AppTestRunner.java
+++ src/test/java/com/ll/AppTestRunner.java
@@ -8,6 +8,7 @@ public class AppTestRunner {
         cmd.stripIndent().trim(); // 이걸 안해주면 안녕, 종료가 안들어가서 오류가 자꾸 발생함
         cmd += "\n종료"; //테스트할 때는 항상 종료로 끝나야 하기 때문에
 
+
         Scanner sc = TestUtil.genScanner(cmd); // stripIndent로 인해서 안녕, 종료가 입력되는 것이다.
         //.stripIndent().trim())
 
@@ -15,10 +16,12 @@ public class AppTestRunner {
 
         new App(sc).run();
 
-        String rs = output.toString();
+        String rs = output.toString(); //App클래스에서 실행하면서 출력되어야 할 결과 문자열들을
+        //출력하지 않고 rs에 저장한다.
 
-        TestUtil.clearSetOutToByteArray(output);
+        TestUtil.clearSetOutToByteArray(output); //출력 금지 중지 -> 이제 화면에 출력이 되게한다.
 
+        System.out.println(rs);
         return rs;
     }
 }
