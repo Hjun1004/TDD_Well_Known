c5f441ab0da598a5fbafcfea1c1077f0f7e0c198
Hjun1004 <hjun1004@naver.com>
2023-03-06 17:47:46 +0900

    AppTestRunner을 도입해서 중복제거

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
+import static com.ll.AppTestRunner.run;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTests {
@@ -37,13 +38,7 @@ public class AppTests {
     @Test
     @DisplayName("프로그램 시작시 타이틀 출력 그리고 종료")
     public void t3() {
-        Scanner sc = TestUtil.genScanner("종료");
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("");
 
         assertThat(rs)
                 .contains("== 명언 앱 ==")
@@ -55,22 +50,11 @@ public class AppTests {
     @Test
     @DisplayName("잘못된 명령어 입력에 대한 처리")
     public void t4() {
-        Scanner sc = TestUtil.genScanner("""
-                안녕
-                종료
-                """.stripIndent().trim()); // stripIndent로 인해서 안녕, 종료가 입력되는 것이다.
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
 
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("안녕\n종료2");
 
         assertThat(rs)
                 .contains("올바르지 않은 명령입니다.");
     }
-
-
-
 
 }
